2da347bc5001a66fa6ca9a5471b3e67e3da433e5
Gilles Major <major.gilles@gmail.com>
2025-04-04 01:32:29 +0200

    wip

--- src/coursera_path/week_1/C1_W1_lecture_nb_01_preprocessing.py
+++ src/coursera_path/week_1/C1_W1_lecture_nb_01_preprocessing.py
@@ -1,11 +1,26 @@
 """Preprocessing."""
 
 import nltk  # Python library for NLP
-from nltk.corpus import twitter_samples  # sample Twitter dataset from NLTK
+import numpy as np
+
+from src.coursera_path.week_1.utils import build_freqs, process_tweet
 
 nltk.download("twitter_samples")
 
-all_positive_tweets = twitter_samples.strings("positive_tweets.json")
-all_negative_tweets = twitter_samples.strings("negative_tweets.json")
+twitter_samples = nltk.corpus.twitter_samples
+tweets = twitter_samples.strings("positive_tweets.json")
+labels = twitter_samples.strings("negative_tweets.json")
 
-print("Number of positive tweets: ", len(all_positive_tweets))
+freqs = build_freqs(tweets, labels)
+X = np.zeros((len(tweets), 3))
+
+
+def extract_features(tweet_components: list[str], freqs: dict) -> np.ndarray:
+    """Extract features from tweet components."""
+    return np.zeros((3,))
+
+
+m = len(tweets)
+for i in range(m):
+    p_tweet = process_tweet(tweets[i])
+    X[i, :] = extract_features(p_tweet, freqs)
--- src/nlp_book/chapter_1.py
+++ src/nlp_book/chapter_1.py
@@ -2,7 +2,7 @@
 
 import re
 
-greetings = " Hi Hello Greetings".split()
+greetings = "Hi Hello Greetings".split()
 user_statement = "Hello Joshua"
 user_token_sequence = user_statement.split()
 print(user_token_sequence)
@@ -20,3 +20,14 @@ r = "(hi|hello|hey|)[ ,:.!]*([a-z]*)"
 print(re.match(r, "hi ho, hi ho, it's off to work ...", flags=re.IGNORECASE))
 print(re.match(r, "Hello Rosa", flags=re.IGNORECASE))
 print(re.match(r, "hey, what's up?", flags=re.IGNORECASE))
+
+r = r"[^a-z]*([y]o|[h']?ello|ok|hey|(good[ ])(morn[gin']{0,3}|"
+r += r"afternoon|even[gin']{0,3}))[\s,;:]{1,3}([a-z]{1,20})"
+re_greeting = re.compile(r, flags=re.IGNORECASE)
+print(re_greeting.match("Hello Rosa"))
+print(re_greeting.match("Hello Rosa").groups())  # type: ignore
+print(re_greeting.match("Good morning Rosa"))
+print(re_greeting.match("Good Manning Rosa"))
+print(re_greeting.match("Good evening Rosa Parks").groups())  # type: ignore
+print(re_greeting.match("Good Morn'n Rosa"))
+print(re_greeting.match("yo Rosa"))
