aba486852c0db05e85e122c27eaa7aaf595e50f0
Gilles Major <major.gilles@gmail.com>
2025-04-04 22:58:14 +0200

    refactor: wip

--- src/nlp_book/chapter_2.py
+++ src/nlp_book/chapter_2.py
@@ -1,1 +1,9 @@
 """Chapter 2: Tokenization."""
+
+text = (
+    "Trust me, though, the words were on their way, and when "
+    "they arrived, Liesel would hold them in her hands like "
+    "the clouds, and she would wring them out, like the rain."
+)
+tokens = text.split()
+print(tokens[:8])
