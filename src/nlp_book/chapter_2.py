efa8b78ef643dc711e33be6386485c997c717c71
Gilles Major <major.gilles@gmail.com>
2025-04-05 10:41:51 +0200

    regex tokenizer

--- src/nlp_book/chapter_2.py
+++ src/nlp_book/chapter_2.py
@@ -1,5 +1,7 @@
 """Chapter 2: Tokenization."""
 
+import re
+
 text = (
     "Trust me, though, the words were on their way, and when "
     "they arrived, Liesel would hold them in her hands like "
@@ -7,3 +9,11 @@ text = (
 )
 tokens = text.split()
 print(tokens[:8])
+
+pattern = r"\w+(?:\'\w+)?|[^\w\s]"
+texts = [text]
+texts.append("There's no such thing as survival of the fittest. " "Survival of the most adequate, maybe.")
+tokens = list(re.findall(pattern, texts[-1]))
+print(tokens[:8])
+print(tokens[8:16])
+print(tokens[16:])
