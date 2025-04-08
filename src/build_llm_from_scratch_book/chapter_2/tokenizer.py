eee090b6191a17533158dcb6863f52cf920ea281
Gilles Major <major.gilles@gmail.com>
2025-04-08 15:49:48 +0200

    refactor: v2 tokenizer

--- src/build_llm_from_scratch_book/chapter_2/tokenizer.py
+++ src/build_llm_from_scratch_book/chapter_2/tokenizer.py
@@ -28,3 +28,29 @@ class SimpleTokenizerV1:
         text = re.sub(r"\s+([,.?!\"()\'])", r"\1", text)  # this is to remove extra spaces before punctuation
         logging.info(f"Decoded text: {text}")
         return text
+
+
+class SimpleTokenizerV2:
+    """Simple tokenizer that splits text into tokens based on whitespace and punctuation.
+
+    Differs from SimpleTokenizerV1 in that it handles unknown tokens and adds special tokens.
+    """
+
+    def __init__(self, vocab: dict[str, int]) -> None:
+        """Initialize the tokenizer with a vocabulary."""
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text: str) -> list[int]:
+        """Encode a text into a list of integers."""
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
+
+        return [self.str_to_int[s] for s in preprocessed]
+
+    def decode(self, ids: list[int]) -> str:
+        """Decode a list of integers into a text."""
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        return re.sub(r'\s+([,.:;?!"()\'])', r"\1", text)
