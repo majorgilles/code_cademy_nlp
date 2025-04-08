00ae07bce6b29259c266f956703662e6d2d1ce90
Gilles Major <major.gilles@gmail.com>
2025-04-08 15:00:09 +0200

    refactor: wip decoding

--- src/build_llm_from_scratch_book/chapter_2/tokenizer.py
+++ src/build_llm_from_scratch_book/chapter_2/tokenizer.py
@@ -18,11 +18,13 @@ class SimpleTokenizerV1:
         """Encode a text into a list of integers."""
         preprocessed = re.split(REGEX_PATTERN, text)
         preprocessed = [token for token in preprocessed if token and token.strip()]
-        return [self.str_to_int[token] for token in preprocessed]
+        result = [self.str_to_int[token] for token in preprocessed]
+        logging.info(f"Encoded text: {result}")
+        return result
 
     def decode(self, tokens: list[int]) -> str:
         """Decode a list of integers into a text."""
         text = " ".join([self.int_to_str[token] for token in tokens])
-        text = re.sub(r"\s+([,.?!\"()\'])", r"\1", text)
+        text = re.sub(r"\s+([,.?!\"()\'])", r"\1", text)  # this is to remove extra spaces before punctuation
         logging.info(f"Decoded text: {text}")
         return text
