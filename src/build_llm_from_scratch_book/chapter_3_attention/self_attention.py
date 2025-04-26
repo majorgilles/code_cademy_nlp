c72277b1798499d311b8a3aeff3d76d15ed2b15e
Gilles Major <major.gilles@gmail.com>
2025-04-26 14:23:42 +0200

    refactor: improve code formatting and add self-attention implementation in chapter 3 notebook

---
+++ src/build_llm_from_scratch_book/chapter_3_attention/self_attention.py
@@ -0,0 +1,30 @@
+"""Compact self-attention implementation from scratch."""
+
+import torch
+from torch import nn
+
+
+class SelfAttentionV1(nn.Module):
+    """Compact self-attention implementation from scratch."""
+
+    def __init__(self, d_in: int, d_out: int) -> None:
+        """Initialize the self-attention layer.
+
+        Args:
+            d_in (int): The size of the embeddings.
+            d_out (int): The size of the output embeddings.
+        """
+        super().__init__()
+        self.W_query = nn.Parameter(torch.randn(d_in, d_out))
+        self.W_key = nn.Parameter(torch.randn(d_in, d_out))
+        self.W_value = nn.Parameter(torch.randn(d_in, d_out))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+
+        attn_scores = queries @ keys.T  # omega
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        return attn_weights @ values
