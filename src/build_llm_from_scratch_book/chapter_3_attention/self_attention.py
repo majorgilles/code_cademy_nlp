3d06d4d1a7844273b1b10cfb2847d11ca820048d
Gilles Major <major.gilles@gmail.com>
2025-04-27 20:02:32 +0200

    feat: implement forward pass for MultiHeadAttention class; add error handling for d_out and enhance tensor operations

--- src/build_llm_from_scratch_book/chapter_3_attention/self_attention.py
+++ src/build_llm_from_scratch_book/chapter_3_attention/self_attention.py
@@ -211,7 +211,8 @@ class MultiHeadAttention(nn.Module):
     ) -> None:
         """Initialize the multi-head attention layer."""
         super().__init__()
-        assert (d_out % num_heads) == 0, "d_out must be divisible by num_heads"
+        if (d_out % num_heads) != 0:
+            raise ValueError("d_out must be divisible by num_heads")
 
         self.d_out = d_out
         self.num_heads = num_heads
@@ -223,8 +224,37 @@ class MultiHeadAttention(nn.Module):
             d_out, d_out
         )  # project the concatenated context vectors back to the original dimension
         self.dropout = nn.Dropout(dropout_ratio)
+        self.mask: torch.Tensor  # to make mypy happy and avoid raising warnings
         self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass that computes context vectors using multi-head attention."""
-        pass
+        batch_size, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(
+            batch_size, num_tokens, self.num_heads, self.head_dim
+        )
+
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vectors = (attn_weights @ values).transpose(1, 2)
+        context_vectors = context_vectors.contiguous().view(batch_size, num_tokens, self.d_out)
+        return self.out_proj(context_vectors)
+
+
+
