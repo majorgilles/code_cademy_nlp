97c1e864e832879f3a4c4424e3c2c2f98ccdd23a
Gilles Major <major.gilles@gmail.com>
2025-05-18 17:51:22 +0200

    feat: implement text generation function in text.py to enhance model capabilities, allowing for diverse output through temperature scaling and top-k sampling techniques

--- src/build_llm_from_scratch_book/text.py
+++ src/build_llm_from_scratch_book/text.py
@@ -31,3 +31,37 @@ def token_ids_to_text(token_ids: torch.Tensor, tokenizer: tiktoken.Encoding) -> 
     """Convert token ids to text."""
     flat = token_ids.squeeze(0)
     return tokenizer.decode(flat.tolist())
+
+
+def generate(  # noqa: PLR0913
+    model: GPTModel,
+    idx: torch.Tensor,
+    max_new_tokens: int,
+    context_size: int,
+    temperature: float = 0.0,
+    top_k: int | None = None,
+    eos_id: int | None = None,
+) -> torch.Tensor:
+    """Generate text with a GPT model.
+
+    A modified text generation function with more diversity
+    """
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id:
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
